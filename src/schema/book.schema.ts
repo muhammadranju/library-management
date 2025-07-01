c14cce5e1202bdc5d702fc8a95fd6138eec78502
Muhammad Ranju <mdranju23@gmail.com>
2025-07-02 01:49:44 +0600

    feat: update createBookSchema to use enum for genre validation

--- src/schema/book.schema.ts
+++ src/schema/book.schema.ts
@@ -3,7 +3,14 @@ import { z } from "zod";
 export const createBookSchema = z.object({
   title: z.string().min(1, "Title is required"),
   author: z.string().min(1, "Author is required"),
-  genre: z.string().min(1, "Genre is required"),
+  genre: z.enum([
+    "FICTION",
+    "NON_FICTION",
+    "SCIENCE",
+    "HISTORY",
+    "BIOGRAPHY",
+    "FANTASY",
+  ]),
   isbn: z.string().min(1, "ISBN is required"),
   description: z.string().optional(),
   copies: z
