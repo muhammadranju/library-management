4563117a996f79463aa807fc8ffc6dc66617d979
Muhammad Ranju <mdranju23@gmail.com>
2025-07-02 00:51:45 +0600

    feat: add routing configuration for books, create book, and borrow summary pages

---
+++ src/routes/index.tsx
@@ -0,0 +1,35 @@
+import App from "@/App";
+import Books from "@/pages/Books";
+import BorrowBooks from "@/pages/BorrowBooks";
+import CreateBook from "@/pages/CreateBook";
+import { Home } from "@/pages/Home";
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    // element: <App />,
+    Component: App,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+
+      {
+        path: "books",
+        Component: Books,
+      },
+      {
+        path: "create-book",
+        Component: CreateBook,
+      },
+      {
+        path: "borrow-summary",
+        Component: BorrowBooks,
+      },
+    ],
+  },
+]);
+
+export default router;
