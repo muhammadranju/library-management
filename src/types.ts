d3b83c67f04611db8feabafa7e35698696e5b1c9
Muhammad Ranju <mdranju23@gmail.com>
2025-07-02 00:51:57 +0600

    feat: initialize project with Vite, React, and TypeScript setup
    - Added package.json with dependencies and scripts for development, building, and linting.
    - Created App.css for styling the application.
    - Implemented main App component with navigation and footer.
    - Added global styles in index.css using Tailwind CSS and custom properties.
    - Set up main entry point in main.tsx with React Router and Redux provider.
    - Defined TypeScript interfaces for tasks, users, books, and borrowing.
    - Configured TypeScript with multiple tsconfig files for app and node environments.
    - Created Vite configuration for React and Tailwind CSS integration.

---
+++ src/types.ts
@@ -0,0 +1,33 @@
+export interface ITask {
+  id: string;
+  title: string;
+  description: string;
+  dueDate: string;
+  isCompleted: boolean;
+  priority: "all" | "high" | "medium" | "low";
+  assignedTo: string | null;
+}
+
+export interface IUser {
+  id: string;
+  name: string;
+  // email: string;
+}
+
+export interface IBook {
+  _id: string;
+  title: string;
+  author: string;
+  genre: string;
+  isbn: string;
+  description?: string;
+  copies: number;
+  available: boolean;
+}
+
+export interface IBorrow {
+  _id: string;
+  bookId: string;
+  quantity: number;
+  dueDate: string;
+}
