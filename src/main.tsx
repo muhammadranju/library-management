d94421ba9c95acc3d42e15788de4a136089143bc
Muhammad Ranju <mdranju23@gmail.com>
2025-07-02 01:49:48 +0600

    feat: add Toaster component for improved notification handling

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,14 @@ import router from "./routes/index.tsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
 import { ThemeProvider } from "./providers/theme-provider.tsx";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Provider store={store}>
         <RouterProvider router={router} />
+        <Toaster />
       </Provider>
     </ThemeProvider>
   </StrictMode>
