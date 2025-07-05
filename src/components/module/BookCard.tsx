da22f922c975e7bd52501760f05e6c0e3de74a94
Muhammad Ranju <mdranju23@gmail.com>
2025-07-05 15:59:29 +0600

    feat: add routing to BookCard component for book details navigation

--- src/components/module/BookCard.tsx
+++ src/components/module/BookCard.tsx
@@ -2,6 +2,7 @@ import { Pencil, Trash2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import type { IBook } from "@/types";
+import { Link } from "react-router";
 
 interface BookCardProps {
   book: IBook;
@@ -17,15 +18,19 @@ export default function BookCard({
   onBorrow,
 }: BookCardProps) {
   return (
-    <Card className="rounded-2xl border shadow-sm">
-      <CardContent className=" space-y-2">
-        <img
-          src={book?.image}
-          alt={book?.title}
-          className="w-full h-96 object-cover rounded-lg"
-        />
-        <div>
-          <h2 className="text-lg font-semibold text-primary">{book.title}</h2>
+    <Card className="rounded-2xl border shadow-sm group">
+      <CardContent className="space-y-2">
+        <Link to={`/books/${book._id}`}>
+          <img
+            src={book?.image}
+            alt={book?.title}
+            className="w-full h-96 object-cover rounded-lg"
+          />
+        </Link>
+        <div className="mt-2">
+          <Link to={`/books/${book._id}`}>
+            <h2 className="text-lg font-semibold text-primary">{book.title}</h2>
+          </Link>
           <p className="text-sm text-muted-foreground">by {book.author}</p>
         </div>
         <p className="text-sm text-muted-foreground">
@@ -48,7 +53,7 @@ export default function BookCard({
         </p>
         <div className="flex items-center justify-between pt-4">
           <Button onClick={() => onBorrow(book)}>Borrow</Button>
-          <div className="flex gap-2">
+          <div className="flex gap-2 opacity-0 group-hover:opacity-100 transition-opacity duration-200">
             <Button variant="outline" onClick={() => onEdit(book)}>
               <Pencil className="w-4 h-4 mr-1" /> Edit
             </Button>
